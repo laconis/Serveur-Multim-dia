9c9b5a5394722a620fddfebbff27211655d19a42
laconis <laconis.dev@gmail.com>
2026-03-12 10:38:17 +0100

    Create MetaDataVideo.cs

---
+++ Models/MetaDataVideo.cs
@@ -0,0 +1,14 @@
+
+
+public MetaDataVideo {
+
+  public int Width{get; set;}
+  public int Width{get; set;}
+  public int Duration{get; set;}
+  public int Codec{get; set;} = "";
+
+  public double Fps{get; set;}  
+  public double Size{get; set;}
+  public long Bitrate{get; set;}
+
+}
